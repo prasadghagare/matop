02cea734c10044293e61cfe5f3b54bc550639494
prasadghagare <ghagarepdg@gmail.com>
2016-08-10 23:19:20 +0530

    Https://github.com/prasadghagare/matop.git

--- matrix.py
+++ matrix.py
@@ -1,5 +1,16 @@
-def fun1():
-	return True
+class Matrix:
+    def __init__(self,name,rows=1,columns=1):
+        self.name = name
+        self.no_of_rows = rows
+        self.no_of_columns = columns
+        self.mat_dict = {}
+        for i in range(1,self.no_of_rows + 1):
+            for j in range(1,self.no_of_columns + 1):
+                self.mat_dict.update({(i,j):1})
 
-value = fun1()
-print value
+
+#testing the class made
+#making a 4*4 matrix with all elements 1
+if __name__ == '__main__':
+    matr_4_4 = Matrix("M",4,4)
+    print matr_4_4.mat_dict
