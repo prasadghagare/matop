294eaa14968bb4e17a19a97b5fcebc5ce1c32ab5
prasadghagare <prasadghagare@Prasads-MacBook-Air.local>
2016-08-18 22:11:27 +0530

    mess cleared
    cleared mess

--- matrix.py
+++ matrix.py
@@ -5,14 +5,13 @@ class Matrix:
         self.no_of_rows = rows
         self.no_of_columns = columns
         self.mat_dict = {}
-        for i in range(1,self.no_of_rows + 1):
-            for j in range(1,self.no_of_columns + 1):
-                self.mat_dict.update({(i,j):1})
-
         self.mat_string = mat_string
         k = self.mat_string.find('[')
+        print 'k',k
         l = self.mat_string.find(']')
+        print 'l',l
         le = len(self.mat_string)
+        print 'le',le
         nums = re.findall(r'[\d]+', self.mat_string)
         #print bool_total_elem
         if k !=0 or l != le - 1 or (self.no_of_columns * self.no_of_rows == len(nums)) == False:
@@ -32,6 +31,6 @@ class Matrix:
 #testing the class made
 #making a 4*4 matrix with all elements 1
 if __name__ == '__main__':
-    matr_4_4 = Matrix("M",4,4)
+   # matr_4_4 = Matrix("M",4,4)
     matr_4_4 = Matrix("M", 3 , 2 ,'[11 2;4 3 :5 33]')
     print matr_4_4.mat_dict
