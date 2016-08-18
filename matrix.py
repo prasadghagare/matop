3be03bb5ae11150726655b41bc4709036af6fa03
prasadghagare <prasadghagare@Prasads-MacBook-Air.local>
2016-08-18 22:04:36 +0530

    clearing mess

--- matrix.py
+++ matrix.py
@@ -1,21 +1,14 @@
-<<<<<<< Updated upstream
-class Matrix:
-    def __init__(self,name,rows=1,columns=1):
-=======
 import re
 class Matrix:
     def __init__(self,name,rows=1,columns=1,mat_string='[1;1]'):
->>>>>>> Stashed changes
         self.name = name
         self.no_of_rows = rows
         self.no_of_columns = columns
         self.mat_dict = {}
-<<<<<<< Updated upstream
         for i in range(1,self.no_of_rows + 1):
             for j in range(1,self.no_of_columns + 1):
                 self.mat_dict.update({(i,j):1})
 
-=======
         self.mat_string = mat_string
         k = self.mat_string.find('[')
         l = self.mat_string.find(']')
@@ -34,14 +27,11 @@ class Matrix:
 
 #    def add():
 #  "overload + operator here"
->>>>>>> Stashed changes
+
 
 #testing the class made
 #making a 4*4 matrix with all elements 1
 if __name__ == '__main__':
-<<<<<<< Updated upstream
     matr_4_4 = Matrix("M",4,4)
-=======
-    matr_4_4 = Matrix("M", 3 , 2 ,'[11 2;4 3 :54 33]')
->>>>>>> Stashed changes
+    matr_4_4 = Matrix("M", 3 , 2 ,'[11 2;4 3 :5 33]')
     print matr_4_4.mat_dict
